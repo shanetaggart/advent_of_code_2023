bcc8709648503bd6953b474cab13640d83ce14be
Shane Taggart <staggart@eastlink.ca>
2023-12-06 13:00:09 -0400

    Refactored solution for puzzle 1 part 1 in perparation for a solution to puzzle 1 part 2.

--- 1 December/dec_1_puzzle_1.py
+++ 1 December/dec_1_puzzle_1.py
@@ -42,14 +42,14 @@ def extract_coordinates(messy_coords_string):
     """
     extract_coordinates( String|messy_coords ): Integer|coords_sum
 
-    This function takes the provided String and returns the sum of the first and last numeral
+    This function takes the provided String and returns the sum of the first and last digit
     found in each line of the provided String.
     
 
     Parameters:
 
     String|messy_coords - A String containing 1 or more lines. Each line may contain letters
-    and numerals.
+    and digits.
     
     
     Return Value(s):
@@ -58,47 +58,34 @@ def extract_coordinates(messy_coords_string):
 
     """
 
-    # Convert the provided String into an Array, each item separated by a carriage return.
+    # Convert the provided String to an Array by separating by a carriage return.
     messy_coords_array = messy_coords_string.split("\n")
     
-    # Create empty Arrays to store the coordinates at various stages of parsing.
-    clean_coords_array = []
-    individual_coords = []
-
+    # Instantiate an empty integer to store the sum of the coordinates.
+    coords_sum = int()
+    
     # Iterate over each line in the Array.
     for line in messy_coords_array:
-        # Create/reset the String, which will store the numerals found in the current line.
-        numerals_in_line = ""
-        # Iterate over every character inside of each line in the Array.
+        # Create a temporary Array to hold the digits found in the current line.
+        digits_in_line = []
+        # Iterate over every character in the current line.
         for char in line:
-            # Conditional to check if the current character is a digit.
+            # Check if the current character is a digit.
             if char.isdigit():
-                # When the current character is a numeral, append it to the String.
-                numerals_in_line += char
-        # When the current Iteration have finished parsing, append the numerals to the Array.
-        clean_coords_array.append(numerals_in_line)
-
-    # Iterate over every coordinate inside of the Array.
-    for coord in clean_coords_array:
-        # Ensure that the current coordinate is not empty.
-        if coord != "":
-            # Store the first and last numeral.
-            new_coord = coord[0] + coord[-1]
-            # Append the new coordinate to the Array.
-            individual_coords.append(int(new_coord))
-
-    # Store the sum of all of the coordinates in the Array, and return it.
-    coords_sum = sum(individual_coords)
+                # If it is a digit, append that digit to the Array
+                digits_in_line.append(char)
+        # Convert the first and last digits to Integers and add them to the sum.
+        coords_sum += int(digits_in_line[0] + digits_in_line[-1])
+    # Return the sum after iterating over all Array items.
     return coords_sum
 
 if __name__ == "__main__":
-    # Store the results of the function for easy formatting.
-    example_output = extract_coordinates(dec_1_puzzle_input.EXAMPLE_INPUT)
-    puzzle_output = extract_coordinates(dec_1_puzzle_input.PUZZLE_INPUT)
+    # Print the example output to the CLI.
+    EXAMPLE_OUTPUT = extract_coordinates(dec_1_puzzle_input.EXAMPLE_INPUT)
+    example_fstring = f"Example Puzzle Input Result: {EXAMPLE_OUTPUT}"
+    print(example_fstring)
 
-    # Create format strings for CLI output.
-    example_fstring = f"Example Puzzle Input Result: {example_output}"
-    puzzle_fstring = f"Puzzle Input Result: {puzzle_output}"
-
-    # Print the result to the CLI.
-    print(example_fstring + "\n" + puzzle_fstring)
+    # Print the puzzle output to the CLI
+    PUZZLE_OUTPUT = extract_coordinates(dec_1_puzzle_input.PUZZLE_INPUT)
+    puzzle_fstring = f"Puzzle Input Result: {PUZZLE_OUTPUT}"
+    print(puzzle_fstring)
